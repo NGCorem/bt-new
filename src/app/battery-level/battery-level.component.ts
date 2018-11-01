3aeb230e7c80051deaf167b2330cb6819fe5bcf4
LAPTOP-K3L38RP3\Corey <corey@automotiveautomation.com.au>
2018-11-01 16:41:24 +1100

    Button2

--- src/app/battery-level/battery-level.component.ts
+++ src/app/battery-level/battery-level.component.ts
@@ -47,35 +47,47 @@ export class BatteryLevelComponent implements OnInit {
       this.getDeviceStatus();
       this.streamValues();
 
-	let deferredPrompt;
-    var btnAdd = document.querySelectorAll('.addToHome')[0];
+var deferredPrompt;
+var btnSave = document.querySelectorAll('.addToHome')[0];
 
-	window.addEventListener('beforeinstallprompt', (e) => {
-	  // Prevent Chrome 67 and earlier from automatically showing the prompt
-	  e.preventDefault();
-	  // Stash the event so it can be triggered later.
-	  deferredPrompt = e;
-	  // Update UI notify the user they can add to home screen
-	  btnAdd.style.display = 'block';
-	});
+    window.addEventListener('beforeinstallprompt', function(e) {
+      console.log('beforeinstallprompt Event fired');
+      //e.preventDefault();   //I even try with this uncommented no luck so far
 
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
 
-	btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+      return false;
     });
-});
+
+    btnSave.addEventListener('click', function() {
+      if(deferredPrompt !== undefined) {
+        // The user has had a postive interaction with our app and Chrome
+        // has tried to prompt previously, so let's show the prompt.
+        deferredPrompt.prompt();
+
+        // Follow what the user has done with the prompt.
+        deferredPrompt.userChoice.then(function(choiceResult) {
+
+          console.log(choiceResult.outcome);
+
+          if(choiceResult.outcome == 'dismissed') {
+            console.log('User cancelled home screen install');
+          }
+          else {
+            console.log('User added to home screen');
+          }
+
+          // We no longer need the prompt.  Clear it up.
+          deferredPrompt = null;
+        });
+      }
+    });
+
+    window.addEventListener('appinstalled', (evt) => {
+      console.log("INSTALLED");
+    });
+
 
     }
 
