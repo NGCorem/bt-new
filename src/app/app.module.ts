1b2a4eab4e4480433144059d07fef10daf106fef
LAPTOP-K3L38RP3\Corey <corey@automotiveautomation.com.au>
2018-11-01 15:40:52 +1100

    NEw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BatteryLevelService } from './battery-level.service'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
+import { BatteryLevelComponent } from './battery-level/battery-level.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+//Comment
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatteryLevelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebBluetoothModule.forRoot({
+      enableTracing: true
+    }),
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [BatteryLevelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
